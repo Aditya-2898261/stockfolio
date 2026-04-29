310d90916d4377df3f2e184b4bd45134ffdc1f63
Aditya-2898261 <adithyat14072005@gmail.com>
2026-04-29 13:35:11 +0530

    add user model with passport-local-mongoose

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  
+  email:{
+    type:String,
+    required:true,
+  },
+  balance:{
+    type:Number,
+    required:true,
+  }
+  
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
