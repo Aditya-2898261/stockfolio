4ce7551d91bee8d13cfaedabc3809967e100da9c
Aditya-2898261 <adithyat14072005@gmail.com>
2026-05-03 12:28:29 +0530

    setup express and passport session and add user with authentication  routes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,50 @@
 const express = require ("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
 const mongoose = require('mongoose');
 const Stock = require("./models/Stock");
+const User = require("./models/user.js");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const userRouter = require("./routes/user.js");
+
+app.use(cors());
+app.use(express.json());
 
 main().catch(err => console.log(err));
-
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/zerodha');
   console.log("DB connected");
 }
 
+
+
+const sessionOptions = {
+  secret: 'Mystocksecreat',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    expires:Date.now()+7*24*60*60*1000,
+    maxAge:7*24*60*60*1000,
+    httpOnly: true,
+  }
+}
+
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get("/",(req,res)=>{
     res.send("hello zerodha");
 })
+
+app.use("/users",userRouter);
 
 app.get("/stocks",async(req,res)=>{
     try {
---
+++ backend/routes/user.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const passport = require("passport");
+
+router.post("/signup",async(req,res) => {
+    let {username,email,password,balance} = req.body;
+    let newUser = new User({username,email,balance});
+    let registerdUser = await User.register(newUser,password);
+    console.log(registerdUser);
+    res.send("user signedup successfully");
+});
+
+router.post("/login",
+     passport.authenticate('local', { failureRedirect: '/login'}),
+     async(req,res) =>{
+    res.send("user loggedin succesfully");
+});
+
+module.exports = router;
