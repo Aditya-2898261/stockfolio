201a03e69c6f5145f0bee14f8f1ebea66de4c6d1
Aditya-2898261 <adithyat14072005@gmail.com>
2026-05-05 04:40:36 +0530

    add Holding model

---
+++ backend/models/holding.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const holdingSchema = new mongoose.Schema({ 
+ user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+stock:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Stock'
+      },
+quantity:{
+    type:Number
+}
+});
+
+
+const Holding = mongoose.model('Holding', holdingSchema);
+
+module.exports = Holding;
