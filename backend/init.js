1edd74ab3f47f25ba63b4a0284fed5847da47b49
Aditya-2898261 <adithyat14072005@gmail.com>
2026-06-29 13:39:37 +0530

    add more stocks to init data

--- backend/init.js
+++ backend/init.js
@@ -10,23 +10,22 @@ async function main() {
 
 
 let fakedata = [
-    {
-    name:"tata",
-    symbol:"TATA",
-    price:456
-    },
-    {
-    name:"zudio",
-    symbol:"ZA",
-    price:346
-    },
-    {
-    name:"deloit",
-    symbol:"DOT",
-    price:873
-    },
+  { name: "Tata Motors", symbol: "TATAMOTORS", price: 456 },
+  { name: "Zudio", symbol: "ZUDIO", price: 346 },
+  { name: "Deloitte", symbol: "DOT", price: 873 },
+  { name: "Reliance Industries", symbol: "RELIANCE", price: 2956 },
+  { name: "Infosys", symbol: "INFY", price: 1823 },
+  { name: "HDFC Bank", symbol: "HDFCBANK", price: 1678 },
+  { name: "TCS", symbol: "TCS", price: 3945 },
+  { name: "Wipro", symbol: "WIPRO", price: 456 },
+  { name: "Adani Ports", symbol: "ADANIPORTS", price: 1234 },
+  { name: "Bajaj Finance", symbol: "BAJFINANCE", price: 7823 },
+  { name: "Asian Paints", symbol: "ASIANPAINT", price: 2876 },
+  { name: "Maruti Suzuki", symbol: "MARUTI", price: 10234 },
+  { name: "Sun Pharma", symbol: "SUNPHARMA", price: 1456 },
+  { name: "ITC Limited", symbol: "ITC", price: 478 },
+  { name: "Larsen & Toubro", symbol: "LT", price: 3567 },
 ];
-
 
 await Stock.insertMany(fakedata);
 console.log("data inserted");
