0007f899284cabaf6fb2367d9bfb5e43d7450823
Aditya-2898261 <adithyat14072005@gmail.com>
2026-07-05 18:45:05 +0530

    fix cookie settings for production

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,8 @@ const sessionOptions = {
     expires:Date.now()+7*24*60*60*1000,
     maxAge:7*24*60*60*1000,
     httpOnly: true,
-     sameSite: false,
+    sameSite: "none", 
+    secure: true,
   }
 }
 
